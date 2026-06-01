56a7fd2f3dabdc3cd4924370bed8026ba0a099f1
Carina <carinalifschitz@gmail.com>
2026-06-01 12:38:32 -0300

    Configurar Vercel para Backend en Python

--- src/components/ValidacionReceta.js
+++ src/components/ValidacionReceta.js
@@ -30,7 +30,7 @@ export default function ValidacionReceta() {
     setResultado("");
 
     try {
-      const respuesta = await fetch("http://127.0.0.1:5000/api/validar", {
+      const respuesta = await fetch("fetch('/api/validar')", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
